faac5341c8866411660c2edcc45ce01437e20894
Tobias Munk <schmunk@usrbin.de>
2016-04-11 14:44:37 +0200

    added Catalog request

---
+++ src/DockerRegistryApi/Request/Catalog.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Madkom\DockerRegistryApi\Request;
+
+use Madkom\DockerRegistryApi\Request;
+
+/**
+ * Class ImageTags
+ * @package Madkom\DockerRegistryApi\Request
+ * @author  Tobias Munk <tobias@diemeisterei.de>
+ * @since   0.8.0
+ */
+class Catalog implements Request
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function method()
+    {
+        return 'GET';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function uri()
+    {
+        return '/v2/_catalog';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function headers()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function data()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function scope()
+    {
+        return '/';
+    }
+
+
+}
